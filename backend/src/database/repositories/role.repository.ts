47364ff6dfdb13b7836a129323865cef522f6665
2kamrul <3kamrulislam@gmail.com>
2024-12-19 01:24:13 +0600

    role permission added

--- backend/src/database/repositories/role.repository.ts
+++ backend/src/database/repositories/role.repository.ts
@@ -8,12 +8,4 @@ export class RoleRepository extends Repository<RoleEntity> {
   constructor(@InjectRepository(RoleEntity) repository: Repository<RoleEntity>) {
     super(repository.target, repository.manager, repository.queryRunner);
   }
-
-  async getOneWithPermissions(id: number): Promise<RoleEntity> {
-    return this.createQueryBuilder("role")
-      .select(["role.id", "role.name", "permissions.id", "permissions.role_id", "permissions.permission_id"])
-      .where("role.id = :id", { id })
-      .leftJoin("role.permissions", "permissions")
-      .getOne();
-  }
 }
