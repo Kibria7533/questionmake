82b609e72d029864008843ff112fe61e49d03729
kibria <tenminuteversity@gmail.com>
2024-12-18 18:22:08 +0600

    added

---
+++ frontend/src/app/questiondownload/page.jsx
@@ -0,0 +1,9 @@
+import QuestionDownload from "@/components/questiondownload";
+import React from "react";
+
+
+const QuestionDownloadPage = () => {
+ return <QuestionDownload/>
+};
+
+export default QuestionDownloadPage;
